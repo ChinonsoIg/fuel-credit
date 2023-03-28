abca873ea23ac805294cb9368314508b2c121e38
ChinonsoIg <ojononso@yahoo.com>
2023-03-28 23:47:07 +0100

    build navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />}>
+            <Route index element={<Login />} />
+          </Route>
+          <Route path="register" element={<Register />}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styles from './../assets/styles/Navbar.module.css';
+import logo from '../assets/images/logo.png'
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className={styles.navbar}>
+      <NavLink to="/" className={styles.logo}>
+        <img src={logo} alt='Logo' />
+      </NavLink>
+      <div className={styles.navbar_grid_container}>
+        <div className={styles.navbar_grid_left}>
+          <NavLink
+            to="/#individuals-corporates"
+            className={({ isActive }) => `
+              ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+            }
+          >
+            Individuals/Corporates
+          </NavLink>
+          <NavLink
+            to="/#merchants"
+            className={({ isActive }) => `
+            ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+            }
+          >
+            Merchants
+          </NavLink>
+        </div>
+        <div className={styles.navbar_grid_right}>
+          <NavLink
+            to="/#about-us"
+            className={({ isActive }) => `
+            ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+            }
+          >
+            About Us
+          </NavLink>
+          <NavLink
+            to="/#faqs"
+            className={({ isActive }) => `
+            ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+            }
+          >
+            FAQs
+          </NavLink>
+          <div className={styles.auth_btn_container}>
+            <button>Login</button>
+            <button>Cretae account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
