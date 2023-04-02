28899dbdababae36a69725f02771c6391ad45b91
ChinonsoIg <ojononso@yahoo.com>
2023-04-02 20:19:56 +0100

    make unauthenticated Navbar mobile responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,8 @@
-import { useSelector } from "react-redux"
-import { selectCurrentUser } from "../features/auth/authSlice"
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, NavLink } from "react-router-dom";
+import { selectCurrentUser } from "../features/auth/authSlice";
+import { AiOutlineBars } from "react-icons/ai";
 
 import styles from "./../assets/styles/Navbar.module.css";
 import logo from "../assets/images/logo.png";
@@ -7,12 +10,15 @@ import bell from "../assets/images/bell.png";
 import file from "../assets/images/file.png";
 import down_stroke from "../assets/images/down_stroke.png";
 import person_transparent from "../assets/images/person_transparent.png";
-import { Link, NavLink } from "react-router-dom";
 import Button from "./Button";
 
 const Navbar = () => {
   const user = useSelector(selectCurrentUser);
+  const [showNavbar, setShowNavbar] = useState(false);
 
+  const handleNavbar = () => {
+    setShowNavbar(!showNavbar);
+  }
 
   if (user) {
     return (
@@ -36,6 +42,53 @@ const Navbar = () => {
       <NavLink to="/" className={styles.logo}>
         <img src={logo} alt="Logo" />
       </NavLink>
+
+      {/* For mobile view */}
+      <div className={styles.toggle_bars} onClick={handleNavbar}>
+        <AiOutlineBars size={20} color="#31581C" />
+      </div>
+      {
+        showNavbar && (
+          <div className={styles.navbar_grid_container_mobile}>
+            <NavLink
+              to="/#individuals-corporates"
+              className={({ isActive }) => `
+              ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+              }
+            >
+              Individuals/Corporates
+            </NavLink>
+            <NavLink
+              to="/#merchants"
+              className={({ isActive }) => `
+            ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+              }
+            >
+              Merchants
+            </NavLink>
+            <NavLink
+              to="/#about-us"
+              className={({ isActive }) => `
+            ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+              }
+            >
+              About Us
+            </NavLink>
+            <NavLink
+              to="/#faqs"
+              className={({ isActive }) => `
+            ${isActive} ? ${styles.navbar_active} : ${styles.navbar_link}`
+              }
+            >
+              FAQs
+            </NavLink>
+            <Link to="/login"><Button title="Log In" variant="transparent" height="45px" /></Link>
+            <Link to="/register"><Button title="Create free account" variant="solid" height="45px" /></Link>
+
+          </div>
+        )
+      }
+
       <div className={styles.navbar_grid_container}>
         <div className={styles.navbar_grid_left}>
           <NavLink
