04b50bde1e7f11336cc0f5014215bfaaa539422c
ChinonsoIg <ojononso@yahoo.com>
2023-04-05 22:38:17 +0100

    redirect from landing page to dashboard if user exists

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,15 +1,30 @@
-import React from 'react'
-import Navbar from '../components/Navbar';
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectCurrentUser} from "../features/auth/authSlice";
+import Navbar from "../components/Navbar";
 
 const Landing = () => {
+  const navigate = useNavigate();
+  const user = useSelector(selectCurrentUser);
+
+  useEffect(() => {
+    if(user) {
+      navigate("/home")
+    }
+  }, [user])
+
+
   return (
     <div>
       <Navbar />
-      <div style={{ maxWidth: "1440px", padding: "0 50px" }}>
+      <div style={{ padding: "0 50px" }}>
       <h1>FuelCredit Landing Page</h1>
       </div>
     </div>
   )
+
+
 }
 
 export default Landing;
